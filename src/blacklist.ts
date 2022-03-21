99f217e4c36e023296369dfbd6cc3e8f3a34dbc6
tfkhdyt <ta142fa@gmail.com>
2022-03-22 05:38:45 +0700

    update dependencies and add more blacklist duration to Aziz

--- src/blacklist.ts
+++ src/blacklist.ts
@@ -2,7 +2,7 @@ export const blackList = [
   {
     username: 'AzizNaufal',
     reason: 'Spam report',
-    until: new Date('2022-03-22T06:00:00+07:00'),
+    until: new Date('2022-03-22T09:00:00+07:00'),
   },
   /*{
     username: 'tfkhdyt',
