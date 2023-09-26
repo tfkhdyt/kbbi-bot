745f8d0513d121e8fcdbefea22546812e2d4a043
tfkhdyt <me@tfkhdyt.my.id>
2023-09-26 12:53:50 +0700

    fix(config): change start and help message

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,15 +7,11 @@ export default {
   message: {
     start: `Selamat datang di bot *KBBI*
 
-Dengan bot ini kalian dapat mencari pengertian dari suatu kata dalam KBBI, kalian hanya tinggal mengirim sebuah kata kepada bot ini maka bot ini akan mengirim pengertian dari kata tersebut.
+Dengan bot ini kalian dapat mencari pengertian dari suatu kata dalam KBBI\\. Kalian hanya perlu mengirim sebuah kata kepada bot ini, maka bot ini akan mengirim definisi dari kata Tersebut.
 
-Data dari bot ini diambil dari situs resmi KBBI (https://kbbi.kemdikbud.go.id/) melalui metode _Web Scraping_ dengan *Node.js*
+Data dari bot ini diambil dari situs resmi KBBI melalui metode _Web Scraping_ dengan *Bun.js*, *Telegraf*, dan *Cheerio*.
 
-Bot ini dibuat menggunakan *TypeScript*, *Node.js*, *Telegraf*, *Axios*, dan *Cheerio*
-
-Bot ini bersifat _open source_ dengan lisensi *GPL-3.0*, kalian dapat melihat source code dari bot ini pada link [berikut](https://github.com/tfkhdyt/kbbi-bot)`,
-    help: `Untuk menggunakan bot ini, kalian hanya perlu mengirimkan kata yang ingin kalian cari kepada bot ini
-
-Jika ada pertanyaan lebih lanjut, silakan tanya langsung kepada saya @tfkhdyt`,
+Untuk pertanyaan lebih lanjut, silakan hubungi @tfkhdyt\\.`,
+    help: 'Untuk menggunakan bot ini, kalian hanya perlu mengirimkan kata yang ingin kalian cari kepada bot ini.\nUntuk pertanyaan lebih lanjut, silakan hubungi @tfkhdyt.',
   },
 }
