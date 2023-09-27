f28e0c18f46a22a94946c2abc7eb0a81d684bd3e
tfkhdyt <me@tfkhdyt.my.id>
2023-09-27 18:18:13 +0700

    refactor(migrate): remove unneeded async function

--- src/db/postgres/migrate.ts
+++ src/db/postgres/migrate.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@libsql/client'
 import { drizzle } from 'drizzle-orm/libsql'
 import { migrate } from 'drizzle-orm/libsql/migrator'
+
 import config from '../../config/config'
 
 const client = createClient({
@@ -9,17 +10,13 @@ const client = createClient({
 })
 const db = drizzle(client)
 
-async function main() {
-  try {
-    await migrate(db, {
-      migrationsFolder: 'drizzle',
-    })
-    console.log('Tables migrated!')
-    process.exit(0)
-  } catch (error) {
-    console.error('Error performing migration: ', error)
-    process.exit(1)
-  }
+try {
+  await migrate(db, {
+    migrationsFolder: 'drizzle',
+  })
+  console.log('Tables migrated!')
+  process.exit(0)
+} catch (error) {
+  console.error('Error performing migration: ', error)
+  process.exit(1)
 }
-
-main()
