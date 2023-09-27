2ea587cc3af2dd30a326e0ea2faf65893a92b2c7
tfkhdyt <me@tfkhdyt.my.id>
2023-09-27 18:18:30 +0700

    refactor(handler): rename saldoHandler to saldo.handler

--- src/handlers/saldo.handler.ts
+++ src/handlers/saldo.handler.ts
@@ -4,9 +4,9 @@ export const saldoHandler = async (ctx: MyContext) => {
   try {
     const user = ctx.user
 
-    ctx.reply(`Jumlah saldo mu adalah: ${user.credits}`)
+    await ctx.reply(`Jumlah saldo mu adalah: ${user.credits}`)
   } catch (error) {
     console.error(error)
-    ctx.reply(`Terjadi error yang tak terduga!, ${error}`)
+    await ctx.reply(`Terjadi error yang tak terduga!, ${error}`)
   }
 }
