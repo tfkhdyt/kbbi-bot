32747b0ef7398f401ac4976f5b04fea341c1f156
tfkhdyt <me@tfkhdyt.my.id>
2023-09-27 11:52:59 +0700

    chore(drizzle.config): set driver to turso

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,14 @@
+import 'dotenv/config'
+
 import type { Config } from 'drizzle-kit'
+import config from './src/config/config'
 
 export default {
   schema: './src/db/postgres/schemas/*',
   out: './drizzle',
+  driver: 'turso',
+  dbCredentials: {
+    url: config.databaseUrl,
+    authToken: config.databaseAuthToken,
+  },
 } satisfies Config
