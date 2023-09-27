ab4785d2f4d020ccf416ce265dd9a1d63b5c616b
tfkhdyt <me@tfkhdyt.my.id>
2023-09-27 15:13:12 +0700

    fix(migrate): create separate client for migration

--- src/db/postgres/migrate.ts
+++ src/db/postgres/migrate.ts
@@ -1,6 +1,16 @@
+import { createClient } from '@libsql/client/.'
+import { drizzle } from 'drizzle-orm/libsql'
 import { migrate } from 'drizzle-orm/libsql/migrator'
-import { db } from '.'
+
+import config from '../../config/config'
 
 console.log('Running migration...')
+
+const client = createClient({
+  url: config.databaseUrl,
+  authToken: config.databaseAuthToken,
+})
+const db = drizzle(client)
 await migrate(db, { migrationsFolder: './drizzle' })
+
 console.log('Migrations is done')
