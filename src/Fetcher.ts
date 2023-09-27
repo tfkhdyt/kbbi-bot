ed4c1e0a000b20066198bc0179312ae101f44ec9
tfkhdyt <me@tfkhdyt.my.id>
2023-09-27 11:24:57 +0700

    fix(fetcher): dont use proxy for a while

--- src/Fetcher.ts
+++ src/Fetcher.ts
@@ -1,14 +1,17 @@
-import config from './config/config.js'
-
 // fetcher class
 export class Fetcher {
-  constructor(private keyword: string) { }
+  constructor(private keyword: string) {}
 
   async getData() {
     try {
+      // const response = await fetch(
+      //   `http://api.scraperapi.com?api_key=${config.scraperAPIKey
+      //   }&url=https://kbbi.kemdikbud.go.id/entri/${this.keyword
+      //     .toLowerCase()
+      //     .trim()}`,
+      // )
       const response = await fetch(
-        `http://api.scraperapi.com?api_key=${config.scraperAPIKey
-        }&url=https://kbbi.kemdikbud.go.id/entri/${this.keyword
+        `https://kbbi.kemdikbud.go.id/entri/${this.keyword
           .toLowerCase()
           .trim()}`,
       )
