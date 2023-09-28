bc7a7e489639760df715dcd5b42c7648e9c2c99c
tfkhdyt <me@tfkhdyt.my.id>
2023-09-28 23:24:53 +0700

    feat(repo/user): add increase credits feature

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,8 +1,8 @@
 import { eq, lt, sql } from 'drizzle-orm'
-import { NewUser } from './../db/postgres/schemas/user.schema'
 
 import { db } from '../db/postgres'
 import { users } from '../db/postgres/schemas/user.schema'
+import { NewUser } from './../db/postgres/schemas/user.schema'
 
 export const findUserByID = async (id: number) =>
   await db.select().from(users).where(eq(users.id, id))
@@ -12,6 +12,12 @@ export const addUser = async (user: NewUser) =>
 
 export const updateUser = async (id: number, user: NewUser) =>
   await db.update(users).set(user).where(eq(users.id, id)).returning()
+
+export const increaseCredits = async (userId: number, amount: number) =>
+  await db
+    .update(users)
+    .set({ credits: sql`${users.credits} + ${amount}` })
+    .where(eq(users.id, userId))
 
 export const decreaseCredits = async (userId: number) =>
   await db
