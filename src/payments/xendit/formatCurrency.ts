9ae9f1e32c24fdc69e8c7aebf47070b57ab2a845
tfkhdyt <me@tfkhdyt.my.id>
2023-09-29 10:25:48 +0700

    fix(formatCurrency): disable maximumSignificantDigits

--- src/payments/xendit/formatCurrency.ts
+++ src/payments/xendit/formatCurrency.ts
@@ -1,7 +1,6 @@
 const formatter = new Intl.NumberFormat('id-ID', {
   style: 'currency',
   currency: 'IDR',
-  maximumSignificantDigits: 1,
 })
 
 export const formatCurrency = (nominal: number) => formatter.format(nominal)
